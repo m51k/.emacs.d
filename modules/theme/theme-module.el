caa55e4592ad43469871598ca6092084bde6756b
m51k <mekail@gmail.com>
2023-11-02 08:09:51 +0530

    changed theme to catppuccin mocha

--- modules/theme/theme-module.el
+++ modules/theme/theme-module.el
@@ -3,17 +3,13 @@
 ;; Configure package doom-themes
 ;;; Code:
 
-(use-package doom-themes
+(use-package catppuccin-theme
   :ensure t
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  (setq doom-themes-treemacs-theme "doom-colors")
-  (doom-themes-treemacs-config)
-  (doom-themes-org-config))
+  (load-theme 'catppuccin :no-confirm)
+  :custom
+  (catppuccin-flavour 'mocha)
+  (catppuccin-reload))
 
 (use-package solaire-mode
   :ensure t
