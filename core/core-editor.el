f14bd833072e90573ea45b3a88da4ce6d71f7d41
m51k <mekail@gmail.com>
2023-11-03 04:28:09 +0530

    startup optimization

--- core/core-editor.el
+++ core/core-editor.el
@@ -3,6 +3,21 @@
 ;; General editor settings
 ;;; Code:
 
+
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+(defun mk/display-startup-time ()
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                     (time-subtract after-init-time before-init-time)))
+           gcs-done))
+(add-hook 'emacs-startup-hook #'mk/display-startup-time)
+
+(use-package no-littering)
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
 (setq inhibit-startup-message 1)
 (setq make-backup-files nil) ; stop creating ~ files
 (setq create-lockfiles nil)
