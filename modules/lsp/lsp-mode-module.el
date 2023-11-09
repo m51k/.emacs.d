38950ba8d02001b29cadc1263b03c3d0c8cf24dc
m51k <mekail@gmail.com>
2023-11-09 14:53:27 +0530

    added prettier-js formatting for jsx and tsx files

--- modules/lsp/lsp-mode-module.el
+++ modules/lsp/lsp-mode-module.el
@@ -5,7 +5,7 @@
 
 (use-package web-mode
   :ensure t
-  :mode ("\\.html?\\'" "\\.css?\\'" "\\.jsx?\\'" "\\.tsx?\\'")
+  :mode ("\\.html?\\'" "\\.css?\\'" "\\.js?\\'" "\\.jsx?\\'" "\\.tsx?\\'")
   :config
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-auto-closing t))
@@ -32,6 +32,21 @@
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
+
+(use-package prettier-js)
+
+(defun enable-minor-mode (my-pair)
+  "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
+  (if (buffer-file-name)
+      (if (string-match (car my-pair) buffer-file-name)
+      (funcall (cdr my-pair)))))
+
+(add-hook 'web-mode-hook #'(lambda ()
+                            (enable-minor-mode
+                             '("\\.jsx?\\'" . prettier-js-mode))))
+(add-hook 'web-mode-hook #'(lambda ()
+                            (enable-minor-mode
+                             '("\\.tsx?\\'" . prettier-js-mode))))
 
 (provide 'lsp-mode-module)
 ;;; lsp-mode-module.el ends here
