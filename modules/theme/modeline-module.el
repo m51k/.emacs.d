da874f40c5edf94c17af5cb5dfd719bf6fb67ea3
m51k <mekail@gmail.com>
2023-11-21 16:09:06 +0530

    switched to default mode line and minions

--- modules/theme/modeline-module.el
+++ modules/theme/modeline-module.el
@@ -3,10 +3,43 @@
 ;; Configure package doom-modeline
 ;;; Code:
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 30)))
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :init (doom-modeline-mode 1)
+;;   :custom ((doom-modeline-height 30)))
+
+(defun simple-mode-line-render (left right)
+  "Return a string of `window-width' length.
+Containing LEFT, and RIGHT aligned respectively."
+  (let ((available-width
+         (- (window-total-width)
+            (+ (length (format-mode-line left))
+               (length (format-mode-line right))))))
+    (append left
+            (list (format (format "%%%ds" available-width) ""))
+            right)))
+
+(setq-default
+ mode-line-format
+ '((:eval
+    (simple-mode-line-render
+     ;; Left.
+     (quote ("%e "
+             mode-line-buffer-identification
+             " %l : %c"
+             evil-mode-line-tag
+             "[%*]"))
+     ;; Right.
+     (quote ("%p "
+             mode-line-frame-identification
+             mode-line-modes
+             mode-line-misc-info))))))
+
+(use-package minions
+  :config
+  (setq minions-mode-line-lighter ""
+        minions-mode-line-delimiters '("" . ""))
+  (minions-mode 1))
 
 (provide 'modeline-module)
 ;;; modeline-module.el ends here
