d8df6f414a2c4403f95c999903f244b2b65391e1
m51k <mekail@gmail.com>
2024-03-04 22:54:58 +0500

    removed irrelevant line

--- modules/lsp/lsp-mode-module.el
+++ modules/lsp/lsp-mode-module.el
@@ -18,8 +18,6 @@
    (c++-mode . lsp-deferred)
    (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp-deferred
-  :config
-  (setq lsp-clients-clangd-executable "clangd")
   :custom
   gc-cons-threshold (* 100 1024 1024)
   read-process-output-max (* 1024 1024)
