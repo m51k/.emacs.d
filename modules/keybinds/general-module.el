f62d707973456c23dc28c1bbfe9a3e02d930c1ae
m51k <mekail@gmail.com>
2024-06-04 12:39:27 +0500

    bind to toggle commenting

--- modules/keybinds/general-module.el
+++ modules/keybinds/general-module.el
@@ -28,6 +28,7 @@
     "wj" 'evil-window-down
     "wk" 'evil-window-up
     "wl" 'evil-window-right
+    "cc" 'comment-or-uncomment-region
     "op" 'treemacs
     "/" 'swiper
     "," 'counsel-switch-buffer
