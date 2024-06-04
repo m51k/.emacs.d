4cc31dd1422cbed7cdb2742bc5fe41177fb4095f
m51k <mekail@gmail.com>
2024-06-04 12:39:30 +0500

    cleaned up more

--- modules/evil/evil-module.el
+++ modules/evil/evil-module.el
@@ -6,20 +6,12 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package evil
-  :init
-  (setq evil-want-keybinding nil)
-  (setq evil-want-integration t)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
   :config
   (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
+  :custom
+  (evil-want-keybinding nil)
+  (evil-want-integration t)
+  (evil-want-C-u-scroll t)
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
