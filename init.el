053d9d5b3ce2340abb43952a4e4c593d2abb8b79
m51k <mekail@gmail.com>
2024-10-16 03:31:32 +0530

    initial commit

---
+++ init.el
@@ -0,0 +1,210 @@
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+
+(setq inhibit-startup-message 1)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 0)
+(menu-bar-mode -1)
+
+(set-face-attribute 'default nil :font "Jetbrains Mono" :height 100)
+
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-hl-line-mode)
+
+(use-package no-littering)
+
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
+(setq mode-line-position (list "(%l:%C) %p of %I "))
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client-mode
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-indentifcation
+                " "
+                mode-line-buffer-identification
+		"  "
+		mode-line-position
+                " "
+                mode-line-modes
+                vc-mode
+		" "
+		"%-"
+                mode-line-misc-info
+                mode-line-end-spaces))
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package general
+  :config
+  (general-auto-unbind-keys)
+
+  (general-create-definer global-leader
+    :prefix "C-c")
+  (global-leader
+    "t"  'eshell
+    "ec" 'flymake-start
+    "s"  'consult-line
+    "es" 'consult-flymake))
+
+(use-package which-key
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package vertico
+  :init
+  (vertico-mode)
+  :config
+  (setq vertico-cycle t)
+  (setq vertico-resize nil))
+
+(savehist-mode 1)
+
+(use-package marginalia
+  :after vertico
+  :config
+  (marginalia-mode 1))
+
+(use-package orderless
+  :demand t
+  :config
+  (setq completion-styles '(orderless basic)))
+
+(use-package consult
+  :config
+  (setq consult-narrow-key "?")
+  :general
+  (global-leader
+   "b" 'consult-buffer))
+
+(use-package embark
+  :bind (("C-." . embark-act)))
+
+(use-package embark-consult)
+
+(use-package corfu
+  :bind (:map corfu-map
+	      ("TAB" . corfu-insert)
+	      ([tab] . corfu-insert)
+	      ("RET" . nil))
+  :custom
+  (completion-category-overrides '((eglot (styles orderless))))
+  (completion-category-defaults nil)
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-preview-current nil)
+  (corfu-quit-at-boundary t)
+  (corfu-quit-no-match t)
+  :config
+  (global-corfu-mode))
+
+(setq electric-pair-pairs '(
+                            (?\{ . ?\})
+                            (?\( . ?\))
+                            (?\[ . ?\])
+                            (?\" . ?\")
+                            ))
+(electric-pair-mode t)
+
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :config
+  (setq org-src-fontify-natively t)
+  (setq org-src-tab-acts-natively t)
+  (setq org-agenda-files '("~/Org/agenda.org"))
+  :general
+  (global-leader
+   "a" 'org-agenda))
+
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Documents/Notes/100 Zettelkasten")
+  (org-roam-capture-templates
+   '(("d" "default"
+      plain (file "~/Documents/Notes/400 Templates/base-zettel.org")
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+TITLE: ${title}\n#+CREATED: %U\n#+MODIFIED: %U\n\n")
+      :unnarrowed t)))
+  :config
+  (org-roam-setup)
+  :general
+  (global-leader
+   "nl" 'org-roam-buffer-toggle
+   "nf" 'org-roam-node-find
+   "ni" 'org-roam-node-insert))
+
+(use-package magit
+  :after general
+  :general
+  (global-leader
+    "gs" 'magit))
+
+(use-package project)
+
+(use-package consult-project-extra
+  :after general
+  :general
+  (global-leader
+    "pf" 'consult-project-extra-find
+    "po" 'consult-project-extra-find-other-window))
+
+(use-package moe-theme
+  :config
+  (load-theme 'moe-dark :no-confirm))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(consult-project-extra magit org-roam general which-key vertico orderless no-littering moe-theme marginalia embark-consult corfu auto-package-update)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
